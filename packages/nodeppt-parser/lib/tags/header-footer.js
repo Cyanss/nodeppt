473ba4c0741aab647961e52970c471500c8cb18b
Cyan <snow22314@outlook.com>
2024-01-03 14:06:53 +0800

    footer header 支持 image背景

--- packages/nodeppt-parser/lib/tags/header-footer.js
+++ packages/nodeppt-parser/lib/tags/header-footer.js
@@ -1,30 +1,87 @@
 const utils = require('./utils');
+const {getAttrs, getAttrsString} = require('../markdown/attrs/utils');
 
 module.exports = tree => {
-    let {slideNode, wrapNode} = utils(tree);
+    let { slideNode, wrapNode } = utils(tree);
 
     if (wrapNode.content && wrapNode.content.length) {
         // console.log(wrapNode.content)
 
-        wrapNode.content = wrapNode.content.map(n => {
-            if (n.tag === 'footer' || n.tag === 'header') {
-                n.content = [
-                    {
-                        tag: 'div',
-                        attrs: {
-                            class: 'wrap'
-                        },
-                        content: n.content
+        wrapNode.content = wrapNode.content.map(node => {
+            if (node.tag === 'footer' || node.tag === 'header') {
+
+                node.content = [{
+                    tag: 'div',
+                    attrs: {
+                        class: 'wrap'
+                    },
+                    content: node.content
+                }];
+
+                const attrs = node.attrs;
+
+                if (attrs && attrs.image) {
+                    let [image, ...imgAttrs] = attrs.image.split(/\s+/);
+                    imgAttrs = getAttrs(`{${imgAttrs.join(' ')}}`, 0, {
+                        leftDelimiter: '{',
+                        rightDelimiter: '}'
+                    });
+                    const rs = {};
+                    let cls = [];
+                    let noBackgroundClass = false;
+                    
+                    if (imgAttrs.length) {
+                        imgAttrs.forEach(([key, value]) => {
+                            if (key === 'class') {
+                                cls = value.split('.').map(c => {
+                                    if (!['dark', 'light', 'anim'].includes(c)) {
+                                        if (
+                                            [
+                                                'top',
+                                                'bottom',
+                                                'right',
+                                                'right-top',
+                                                'right-bottom',
+                                                'center',
+                                                'center-top',
+                                                'center-bottom',
+                                                'left',
+                                                'left-top',
+                                                'left-bottom'
+                                            ].includes(c)
+                                        ) {
+                                            noBackgroundClass = true;
+                                        }
+                                        return `background-${c}`;
+                                    }
+
+                                    return c;
+                                });
+                            } else {
+                                rs[key] = value;
+                            }
+                        });
                     }
-                ];
-                if (n.tag === 'header') {
-                    slideNode.content.unshift(n);
+
+                    node.content.unshift({
+                        tag: 'span',
+                        attrs: {
+                            ...rs,
+                            class: [noBackgroundClass ? '' : 'background', ...cls].join(' '),
+                            style: `background-image:url('${image}')`
+                        }
+                    });
+                }
+
+
+                if (node.tag === 'header') {
+                    slideNode.content.unshift(node);
                 } else {
-                    slideNode.content.push(n);
+                    slideNode.content.push(node);
                 }
                 return false;
             }
-            return n;
+            return node;
         });
     }
 };
